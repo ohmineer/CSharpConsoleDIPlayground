9d808fcba13f01a0e8fcb7d7e2097b1bdfc73977
Daniel Serrano <dserrano@teltronic.es>
2021-12-21 17:48:04 +0100

    build: 🔨 Improve PS release script

--- release.ps1
+++ release.ps1
@@ -1,8 +1,9 @@
-[CmdletBinding()]
+[CmdletBinding(DefaultParameterSetName = "set1")]
 param(
   [switch]$Help,
   [switch]$PushRemote,
-  [Parameter()][ValidateSet('Major', 'Minor')][string]$VersionIncrement = 'Minor'
+  [Parameter()][parameter(ParameterSetName = 'set1')][ValidateSet('Major', 'Minor')][string]$VersionIncrement = 'Minor',
+  [Parameter()][parameter(ParameterSetName = 'set2')][string]$NextVersion
 )
 
 Set-StrictMode -Version 2
@@ -13,17 +14,29 @@ if ($Help.IsPresent) {
   exit 0
 }
 
-# Pattern of version produced by prepare
-$tagPattern = "v\d+(?:\.\d+)+"
+if (-not ([string]::IsNullOrWhiteSpace($NextVersion))) {
+  $versionPattern = "^\d+\.\d+$"
+  $matchResult = $NextVersion -match $versionPattern
+  if ($matchResult) {
+    $version = nbgv prepare-release --nextVersion $NextVersion --format json | ConvertFrom-Json
+  }
+  else {
+    Write-Error "Version must be formatted as Major.Minor"
+    exit 0
+  }
+}
+else {
+  $version = nbgv prepare-release --versionIncrement $VersionIncrement --format json | ConvertFrom-Json
+}
 
 # Prepare the release
-$version = nbgv prepare-release --versionIncrement $VersionIncrement --format json | ConvertFrom-Json
 # Create a tag for the new branch in both local repo and origin.
 $tag = nbgv tag $version.NewBranch.Commit
 # Push everything to remote if it exists
 if ((git ls-remote --exit-code origin) -and ($PushRemote.IsPresent)) {
   git push origin
 
+  $tagPattern = "v\d+(?:\.\d+)+"
   $matchResult = $tag -match $tagPattern
   if ($matchResult) {
     git push origin matches[0]
